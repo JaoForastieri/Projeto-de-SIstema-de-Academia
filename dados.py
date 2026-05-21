26236a40b5067749dd00702f5d0da579a688aae4
JaoForastieri <jaobolachaez@gmail.com>
2026-05-21 15:48:06 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -1,8 +1,9 @@
 
+
+
 #listas para armazenar os valores.
 
 lista_alunos = [] #armazenamento dos alunos.
-lista_modalidades = [] #armazenamento das modalidades.
 lista_fila = [] #armazenamento da fila presencial de atendimento.
 pessoas_pago =[] #armazenamento dos clientes que efetuaram o pagamento.
 pessoas_pendente = [] #armazenamento dos clientes que ainda não efetuaram o pagamento.
@@ -10,10 +11,13 @@ lista_checkins = [] #armazenamento da lista dos clientes que fizeram check-in.
 
 
 PLANOS_DISPONIVEIS = {   
-    'Básico': 69.90
-    'Gold': 129.90
+    'Básico': 69.90,
+    'Gold': 129.90,
     'Black': 219.90
 }
 
 STATUS_PAGAMENTO = ('Pendente', 'Pago')
 DIAS_DA_SEMANA = ('Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado', 'Domingo')
+MODALIDADES = ('Musculação', 'Natação', 'Judô', 'Muay Thay', 'Taekwondo', 'Boxe', 'Jiu-Jitsu', 'Spinning')
+
+
