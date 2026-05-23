baacb6b0de0a47b23490e2ff6d04746f714000e7
laurasouzaaya-gif <laurasouzaaya@gmail.com>
2026-05-23 15:29:05 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,12 @@
 import os #import de biblioteca para limpar os terminais
-from tarefas import id_pessoa
 
 #função para limpar o terminal e deixar organizado.
 def limpar():
-    os.system('cls')
+   os.system('cls' if os.name == 'nt' else 'clear')
 
 #função para inserir título e deixar bonito.
-def titulo(str):
-    x = ' '
-    print(f'\n{"-=-" * 15}\n {x*2} {str.upper()}   \n{"-=-" * 15}')
+def titulo(texto):
+    print(f"\n{'—' * 15} {texto.upper()} {'—' * 15}\n")
 
 #função para preparar a tela de entrada que será usada em main.py
 def main():
@@ -30,18 +28,21 @@ def main():
 def menu_modalidade():
     
  titulo('-=-=-=- Menu De Modalidades -=-=-=-')
- while True
+ while True:
   try:
     var = input("1. Musculação\n2. Natação\n3. Judô\n4. boxe\n5. Yoga\n\n Escolha sua modalidade:")
+    if var in ['1', '2', '3', '4', '5']:
+            return var
   except:
     print('Entrada Invalida!')
       
 
 def concluir_sessao():
-    
+
     planos = {
-        Básico = 1,
-        Gold = 3,
-        Black = 5
+        'basico': 1,
+        'gold': 3,
+        'black': 5
     }
 
+    return planos
