ad02140973a9e4c5d14a8372341b6ef88e945395
laurasouzaaya-gif <laurasouzaaya@gmail.com>
2026-05-23 15:31:22 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -6,22 +6,20 @@ lista_fila = [] #armazenamento da fila presencial de atendimento.
 pessoas_pago =[] #armazenamento dos clientes que efetuaram o pagamento.
 pessoas_pendente = [] #armazenamento dos clientes que ainda não efetuaram o pagamento.
 lista_checkins = [] #armazenamento da lista dos clientes que fizeram check-in. 
-fila = [] #armazenamento da 
+fila = [] #armazenamento da fila  
 
-PLANOS_DISPONIVEIS = {   
-    'Básico': 69.90,
-    'Gold': 129.90,
-    'Black': 219.90
-}.lower()
+planos_disponiveis = {   
+    'básico': 69.90,
+    'gold': 129.90,
+    'black': 219.90
+}
 
-STATUS_PAGAMENTO = ('Pendente', 'Pago').lower()
-DIAS_DA_SEMANA = ('Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado', 'Domingo').lower()
-MODALIDADES = {
-    'Musculação' : 1,   
-    'Natação' : 2,
-    'Judô' : 3,
-    'Boxe' : 4,
-    'Yoga' :5
-}.lower()
-
-
+status_pagamento = ('pendente', 'pago')
+dias_da_semana = ('segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado', 'domingo')
+modalidades = {
+    'musculação' : 1,   
+    'natação' : 2,
+    'judô' : 3,
+    'boxe' : 4,
+    'yoga' :5
+}
